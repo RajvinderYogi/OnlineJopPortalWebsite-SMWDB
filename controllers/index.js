13e1db1feca607d02c9f2529c270f4c0d82871c3
RajvinderYogi <rajvinderyogi@yahoo.com>
2018-03-14 14:53:38 -0400

    modified header login/logout

--- controllers/index.js
+++ controllers/index.js
@@ -5,21 +5,22 @@ let User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SMWDB - Home' });
+  res.render('index', { title: 'SMWDB - Home', user: req.user  });
 });
 router.get('/jobSeeker', function(req, res, next) {
-    res.render('jobSeeker', { title: 'SMWDB - Job Seeker' });
+    res.render('jobSeeker', { title: 'SMWDB - Job Seeker', user: req.user });
 });
 router.get('/employer', function(req, res, next) {
-    res.render('employer', { title: 'SMWDB - Employers' });
+    res.render('employer', { title: 'SMWDB - Employers', user: req.user  });
 });
 router.get('/contactUs', function(req, res, next) {
-    res.render('contactUs', { title: 'SMWDB - Contact Us' });
+    res.render('contactUs', { title: 'SMWDB - Contact Us', user: req.user  });
 });
 
 router.get('/register', (req, res, next)=>{
     res.render('register', {
-        title:'SMWDB - Register'
+        title:'SMWDB - Register',
+        user: req.user
     });
 });
 
@@ -47,7 +48,8 @@ router.get('/login', (req, res, next)=>{
 
     res.render('login', {
         title:'SMWDB - Login',
-        messages: messages
+        messages: messages,
+        user: req.user
     });
 });
 
@@ -59,6 +61,10 @@ router.post('/login',
     failureMessage:'Invalid Login'
 }));
 
-
+//GET: /logout
+router.get('/logout', (req, res, next)=>{
+    req.logout();
+    res.redirect('/')
+});
 
 module.exports = router;
