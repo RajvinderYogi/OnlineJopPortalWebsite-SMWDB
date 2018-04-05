59298dde7b154f0d99376a1097a836ff0a5a6773
sukhwinder222 <aulakhsukhwinder2@gmailll.com>
2018-04-04 20:10:47 -0400

    resolved contact form errors

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var express_handlebars = require('express-handlebars');
+const nodemailer = require('nodemailer');
 
 // references we added
 const mongoose = require('mongoose');
