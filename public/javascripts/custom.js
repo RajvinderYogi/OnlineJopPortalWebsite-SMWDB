171c1a54b8d3e2b02f9a776728136c530b67caf6
impankajsaini <impankaj.saini.1947@gmail.com>
2018-04-16 14:12:19 -0400

    Added employment services provides logo and some styling in profile informartion

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -38,3 +38,26 @@ window.onclick = function(event) {
         }
     }
 }
+
+$(document).ready(function(){
+    $('.company-logos').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
